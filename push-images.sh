4111900da9072828597f59ea36c817f81abeba80
kewubenben <32124195+kewubenben@users.noreply.github.com>
2019-07-18 10:41:49 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -6,8 +6,8 @@ imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
 docker pull $imagepath
 
 # push到阿里云仓库
-docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
-docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
+docker tag $imagepath registry.cn-zhangjiakou.aliyuncs.com/daiming/k8s/$imagename
+docker push registry.cn-zhangjiakou.aliyuncs.com/daiming/k8s/$imagename
 
 # push到dockerhub
 docker tag $imagepath willdockerhub/$imagename
