4186bf5e06d9d58e9a772339811dbb3fa2b5a3fb
kewubenben <32124195+kewubenben@users.noreply.github.com>
2019-07-18 10:48:31 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+#push google or coreos images to dockerhub or aliyun registry,
+#change the registry name and username/password to yourself's.
 
-for imagepath in $(cat ./imagepath.txt)
+DOCKER_HUB=k8s
+ALI_REGISTRY=registry.cn-zhangjiakou.aliyuncs.com/daiming/k8s
+
+#config the following env at travis-ui
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login registry.cn-zhangjiakou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+
+for image in $(cat img-list.txt)
 do
-imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
-docker pull $imagepath
-
-# push到阿里云仓库
-docker tag $imagepath registry.cn-zhangjiakou.aliyuncs.com/daiming/k8s/$imagename
-docker push registry.cn-zhangjiakou.aliyuncs.com/daiming/k8s/$imagename
-
-# push到dockerhub
-docker tag $imagepath willdockerhub/$imagename
-docker push willdockerhub/$imagename
+	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	docker pull $image
+	docker tag $image $ALI_REGISTRY/$imagename
+	docker tag $image $DOCKER_HUB/$imagename
+	# push到dockerhub
+	docker push $DOCKER_HUB/$imagename
+	# push到阿里云仓库
+	docker push $ALI_REGISTRY/$imagename
 done
